ce9cd86ac5e64742ebeb7713f85b583e365334db
Vyacheslav L. <Dwek18@mail.ru>
2025-05-05 16:16:38 +0300

    8.7 Date. Отображение даты

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -5,6 +5,6 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        System.out.println(item.getLocalDateTime().format(DateTimeFormatter.ofPattern("dd-MMMM-yyyy HH : mm : ss")));
+        System.out.println(item.getCreated().format(DateTimeFormatter.ofPattern("dd-MMMM-yyyy HH : mm : ss")));
     }
 }

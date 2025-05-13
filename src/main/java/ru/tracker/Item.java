e4365d8c596821c4e37c66e0c88f9f5f960adece
Vyacheslav L. <Dwek18@mail.ru>
2025-05-13 13:39:01 +0300

    9.12 toString()

--- src/main/java/ru/tracker/Item.java
+++ src/main/java/ru/tracker/Item.java
@@ -1,11 +1,13 @@
 package ru.tracker;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Item {
     private int id;
     private String name;
     private final LocalDateTime created = LocalDateTime.now();
+    private final static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-yyyy HH : mm : ss");
 
     public Item() {
     }
@@ -39,4 +41,12 @@ public class Item {
         return created;
     }
 
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", created=" + created.format(FORMATTER) +
+                '}';
+    }
 }

23becd4e81a9b15143c13f5811458429d480dd63
Vyacheslav L. <Dwek18@mail.ru>
2025-05-15 10:33:09 +0300

    10.6 StartUI. Вывод всех заявок

--- src/main/java/ru/tracker/StartUI.java
+++ src/main/java/ru/tracker/StartUI.java
@@ -17,6 +17,12 @@ public class StartUI {
                 Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("Добавленная заявка: " + item);
+            }
+            if (select == 1) {
+                System.out.println("=== Вывод всех заявок ===");
+                for (Item item : tracker.findAll()) {
+                    System.out.println(item);
+                }
             } else if (select == 6) {
                 run = false;
             }

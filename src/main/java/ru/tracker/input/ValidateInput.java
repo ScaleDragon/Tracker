3c8f079e5e8bf7929d4a2982e8a67581c12062a6
Vyacheslav L. <Dwek18@mail.ru>
2025-06-11 11:14:10 +0300

    11_8_Обеспечить_бесперебойную_работу_приложения_Tracker

--- src/main/java/ru/tracker/input/ValidateInput.java
+++ src/main/java/ru/tracker/input/ValidateInput.java
@@ -2,10 +2,10 @@ package ru.tracker.input;
 
 public class ValidateInput extends ConsoleInput {
     @Override
-    public int askInt(String question) throws NumberFormatException {
+    public int askInt(String question) {
         while (true) {
             try {
-                return Integer.parseInt(askStr(question));
+                return super.askInt(question);
             } catch (NumberFormatException e) {
                 System.out.println("Пожалуйста, введите корректные данные");
             }
